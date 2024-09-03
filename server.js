4a2dbdbf7d0a4867ec8241dc0ac99e4de1421c3c
terkinas <52948963+terkinas@users.noreply.github.com>
2024-09-03 16:22:07 +0300

    Update caching for instant file serving
    * updated caching for files
    
    * updated caching
    
    * update
    
    ---------
    
    Co-authored-by: arniz <arnizaq@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const os = require('os');
+const crypto = require('crypto');
 const util = require('util');
 
 const execPromise = util.promisify(exec);
@@ -14,17 +14,41 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Directory to store cached files
+const cacheDir = path.join(__dirname, 'cache');
+
+// Ensure the cache directory exists
+if (!fs.existsSync(cacheDir)) {
+    fs.mkdirSync(cacheDir);
+}
+
+// Function to generate a unique cache key based on the URL
+function generateCacheKey(url) {
+    return crypto.createHash('md5').update(url).digest('hex');
+}
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/download', async (req, res) => {
-    const url = req.body.url; // Extract the URL from the request body
-    console.log(`Requested URL: ${url}`);
+    const url = req.body.url;
 
     if (!url) {
         return res.status(400).json({ error: 'No URL provided' });
     }
+
+    const cacheKey = generateCacheKey(url);
+    const cachedFile = path.join(cacheDir, `${cacheKey}.mp3`);
+
+    // Check if the file is already in the cache
+    if (fs.existsSync(cachedFile)) {
+        console.log('Serving from cache:', cachedFile);
+        return res.download(cachedFile);
+    }
+
+    console.log(`Downloading and converting: ${url}`);
 
     try {
         // Fetch metadata to get the video title
